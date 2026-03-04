1a7097846203c6e42c1d889a04891125535cb93f
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-04 01:20:54 -0800

    Fix when newlines/etc before scriptName

--- Caprica/papyrus/PapyrusCompilationContext.cpp
+++ Caprica/papyrus/PapyrusCompilationContext.cpp
@@ -168,8 +168,9 @@ std::string_view findScriptName(const std::string_view& data, const std::string_
     if (next == std::string_view::npos)
       next = data.size() - 1;
     auto line = data.substr(last, next - last);
-    auto begin = line.find_first_not_of(" \t");
-    if (strnicmp(line.substr(begin, startstring.size()).data(), startstring.data(), startstring.size()) == 0) {
+    auto begin = line.find_first_not_of(" \t\r");
+    if (begin != std::string_view::npos &&
+        strnicmp(line.substr(begin, startstring.size()).data(), startstring.data(), startstring.size()) == 0) {
       auto first = line.find_first_not_of(" \t", startstring.size() + begin);
       return line.substr(first, line.find_first_of(" \t", first) - first);
     }
